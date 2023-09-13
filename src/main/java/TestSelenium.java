0c0fd4a3c75a3872be468ec1bf469deb0ae357b6
TovaMiler <t0583258048@gmail.com>
2023-09-13 11:18:22 +0300

    פרוייקט אגורה

--- src/main/java/TestSelenium.java
+++ src/main/java/TestSelenium.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class TestSelenium {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        // System.setProperty("webdriver.chrome.driver","Drivers/msedgedriver.exe");
+        // WebDriver driver = new EdgeDriver();
+        driver.get("https://www.google.com");
+        //Thread.sleep(5000);
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
 }
